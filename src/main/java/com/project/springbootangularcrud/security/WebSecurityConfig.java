a2a4aad2342c1a8431dd77bd37a9262bbf42c219
Pritam Jadge <pritam.jadge@gmail.com>
2023-09-27 12:41:03 +0530

    add @EnableWebSecurity annotation

--- src/main/java/com/project/springbootangularcrud/security/WebSecurityConfig.java
+++ src/main/java/com/project/springbootangularcrud/security/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,7 +23,7 @@ import com.project.springbootangularcrud.security.jwt.AuthTokenFilter;
 import com.project.springbootangularcrud.security.services.UserDetailsServiceImpl;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(
         // securedEnabled = true,
         // jsr250Enabled = true,
