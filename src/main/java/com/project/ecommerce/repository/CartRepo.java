13ebc0daa7d463bfaba8008dd52796c339633283
Pritam Jadge <pritam.jadge@gmail.com>
2023-10-06 16:38:00 +0530

    added features 1.add to cart 2.fetch cart products count per user

---
+++ src/main/java/com/project/ecommerce/repository/CartRepo.java
@@ -0,0 +1,13 @@
+package com.project.ecommerce.repository;
+
+import com.project.ecommerce.models.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartRepo extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserIdAndProductProductId(Long userId, Long productId);
+
+    List<Cart> findByUser_Id(Long userId);
+}
